d9d725d776c83fb7449e5469161c7d36e43a0e6b
Dung Nguyen Dang <dangdungcntt@gmail.com>
2022-07-28 12:34:54 +0700

    Support music video. Fix #14

--- src/index.js
+++ src/index.js
@@ -9,6 +9,63 @@ const resolvePlayerResponse = (watchHtml) => {
     return matches ? matches[1] + '}}}' : ''
 }
 
+const getJSFile = async(url) => {
+    try {
+        let { data } = await axios.get(url);
+        return data;
+    } catch (e) {
+        return null;
+    }
+}
+
+const buildDecoder = async(watchHtml) => {
+    if (!watchHtml) {
+        return null;
+    }
+
+    let jsFileUrlMatches = watchHtml.match(/\/s\/player\/[A-Za-z0-9]+\/[A-Za-z0-9_.]+\/[A-Za-z0-9_]+\/base\.js/);
+
+    if (!jsFileUrlMatches) {
+        return null;
+    }
+
+
+    let jsFileContent = await getJSFile(`https://www.youtube.com${jsFileUrlMatches[0]}`);
+
+    let decodeFunctionMatches = jsFileContent.match(/function.*\.split\(\"\"\).*\.join\(\"\"\)}/);
+
+    if (!decodeFunctionMatches) {
+        return null;
+    }
+
+    let decodeFunction = decodeFunctionMatches[0];
+
+    let varNameMatches = decodeFunction.match(/\.split\(\"\"\);([a-zA-Z0-9]+)\./);
+
+    if (!varNameMatches) {
+        return null;
+    }
+
+    let varDeclaresMatches = jsFileContent.match(new RegExp(`(var ${varNameMatches[1]}={[\\s\\S]+}};)[a-zA-Z0-9]+\\.[a-zA-Z0-9]+\\.prototype`));
+
+    if (!varDeclaresMatches) {
+        return null;
+    }
+
+    return function(signatureCipher) {
+        let params = new URLSearchParams(signatureCipher);
+        let { s: signature, sp: signatureParam, url } = Object.fromEntries(params);
+        let decodedSignature = eval(`
+            "use strict";
+            ${varDeclaresMatches[1]}
+            (${decodeFunction})("${signature}")
+        `);
+
+        return `${url}&${signatureParam}=${encodeURIComponent(decodedSignature)}`;
+    }
+
+}
+
 const getInfo = async({ url }) => {
 
     let videoId = getVideoId({ url });
@@ -26,13 +83,36 @@ const getInfo = async({ url }) => {
     try {
         let ytInitialPlayerResponse = resolvePlayerResponse(response.data);
         let parsedResponse = JSON.parse(ytInitialPlayerResponse);
-        let streamingData = parsedResponse.streamingData || {}
+        let streamingData = parsedResponse.streamingData || {};
+
+        let formats = (streamingData.formats || [])
+            .concat(streamingData.adaptiveFormats || []);
+
+        let isEncryptedVideo = !!formats.find(it => !!it.signatureCipher);
+
+        if (isEncryptedVideo) {
+            let decoder = await buildDecoder(response.data);
+
+            if (decoder) {
+                formats = formats.map(it => {
+                    if (it.url || !it.signatureCipher) {
+                        return it;
+                    }
+
+                    it.url = decoder(it.signatureCipher);
+                    delete it.signatureCipher;
+                    return it;
+                });
+            }
+        }
 
         return {
             videoDetails: parsedResponse.videoDetails || {},
-            formats: (streamingData.formats || []).concat(streamingData.adaptiveFormats || []).filter(format => format.url)
+            formats: formats
+                .filter(format => format.url)
         }
-    } catch {
+    } catch (e) {
+        console.log(e);
         //Do nothing here
         return false
     }
--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 const Youtube = require('../src');
 
 (async() => {
-    console.log(await Youtube.getInfo({ url: 'https://www.youtube.com/watch?v=pJ7WN3yome4' }))
+    console.log(await Youtube.getInfo({ url: 'https://www.youtube.com/watch?v=weRHyjj34ZE' }))
 })();
